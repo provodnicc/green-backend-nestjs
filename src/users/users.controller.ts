433c75065ae2f032da622d388953eb0ad51844e8
Pavel <pasha1201s@inbox.ru>
2022-12-24 13:06:47 +0300

    add users list

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Role } from 'src/auth/enums/roles.enum';
+import { GetUserDto } from './dtos/get-user.dto';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async getAll(){
+    const users = await this.usersService.getUsers()
+    return users.map((user: User)=>{
+      if(user.role==Role.ADMIN){
+        return
+      }
+      return new GetUserDto(user)
+    })
+  }
 }
